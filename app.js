582f2663a2d131c6fb5d4ee3acc80caff35bff56
Duo Liu <DuoLiu1@Duos-MacBook-Pro-2.local>
2017-11-12 12:19:17 -0500

    add limit

--- app.js
+++ app.js
@@ -309,7 +309,7 @@ function receivedMessage(event) {
       var keys = search_product_key(messageText);
       console.log(keys);
       if (keys) {
-        Product.find({ 'tags': { $in: keys } }, 
+        Product.find({ 'tags': { $in: keys }.limit(5) }, 
         function (err, foundProducts) {
           console.log(foundProducts);
           if (err) {
